e28bbab5dac45bb9b2330437e8d4b9b571d21e49
Sammii-8954 <sblake17a@vtdi.edu.jm>
2024-02-18 22:32:28 -0500

    Add project files.

---
+++ Sensational Scents F/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Sensational_Scents_F
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CustomerNameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DateofOrder = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DeliveryAddressBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ProductDropDown = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CustomerNoBox = new System.Windows.Forms.TextBox();
+            this.CostBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Vivaldi", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(372, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sensational Scents";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // CustomerNameBox
+            // 
+            this.CustomerNameBox.Location = new System.Drawing.Point(12, 123);
+            this.CustomerNameBox.Name = "CustomerNameBox";
+            this.CustomerNameBox.Size = new System.Drawing.Size(157, 20);
+            this.CustomerNameBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Customer Name";
+            // 
+            // DateofOrder
+            // 
+            this.DateofOrder.Location = new System.Drawing.Point(12, 190);
+            this.DateofOrder.Name = "DateofOrder";
+            this.DateofOrder.Size = new System.Drawing.Size(200, 20);
+            this.DateofOrder.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date of order";
+            // 
+            // DeliveryAddressBox
+            // 
+            this.DeliveryAddressBox.Location = new System.Drawing.Point(12, 255);
+            this.DeliveryAddressBox.Name = "DeliveryAddressBox";
+            this.DeliveryAddressBox.Size = new System.Drawing.Size(157, 20);
+            this.DeliveryAddressBox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Delivery Address";
+            // 
+            // ProductDropDown
+            // 
+            this.ProductDropDown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ProductDropDown.FormattingEnabled = true;
+            this.ProductDropDown.Items.AddRange(new object[] {
+            "Scented Candles",
+            "Hand Soaps",
+            "Bath Soaps",
+            "Bath Bombs",
+            "Accessories"});
+            this.ProductDropDown.Location = new System.Drawing.Point(12, 313);
+            this.ProductDropDown.Name = "ProductDropDown";
+            this.ProductDropDown.Size = new System.Drawing.Size(157, 21);
+            this.ProductDropDown.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Product";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.SubmitButton.Font = new System.Drawing.Font("Vivaldi", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubmitButton.Location = new System.Drawing.Point(650, 348);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(105, 43);
+            this.SubmitButton.TabIndex = 12;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = false;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(326, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Customer Number";
+            // 
+            // CustomerNoBox
+            // 
+            this.CustomerNoBox.Location = new System.Drawing.Point(330, 123);
+            this.CustomerNoBox.Name = "CustomerNoBox";
+            this.CustomerNoBox.Size = new System.Drawing.Size(157, 20);
+            this.CustomerNoBox.TabIndex = 14;
+            // 
+            // CostBox
+            // 
+            this.CostBox.Location = new System.Drawing.Point(330, 180);
+            this.CostBox.Name = "CostBox";
+            this.CostBox.Size = new System.Drawing.Size(157, 20);
+            this.CostBox.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(326, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Cost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.CostBox);
+            this.Controls.Add(this.CustomerNoBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ProductDropDown);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DeliveryAddressBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DateofOrder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.CustomerNameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CustomerNameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DateofOrder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox DeliveryAddressBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox ProductDropDown;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox CustomerNoBox;
+        private System.Windows.Forms.TextBox CostBox;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Sensational Scents F/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sensational_Scents_F
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string CustomerName = CustomerNameBox.Text;
+            string OrderDate = DateofOrder.Value.ToString();
+            var product = ProductDropDown.SelectedItem.ToString();
+            string CustomerAddress = DeliveryAddressBox.Text;
+            string customerNumber = CustomerNoBox.Text;
+            double cost = Convert.ToDouble(CostBox.Text);
+            var isValid = true;
+
+            if(string.IsNullOrWhiteSpace(CustomerName) || string.IsNullOrWhiteSpace(product) || string.IsNullOrWhiteSpace(CustomerAddress))
+            {
+                isValid = false;
+                MessageBox.Show("Please enter missing data.");
+
+            }
+            // if (isValid == True)
+            if(isValid)
+            {
+                MessageBox.Show($"Customer Name: {CustomerName}\n\r" +
+                $"Date of Order: {OrderDate}\n\r" +
+                $"Delievery Address: {CustomerAddress}\n\r" +
+                $"Cost: {cost}\n\r" +
+                $"Customer Number: {customerNumber}\n\r" +
+                $"Product: {product}\n\r" +
+                $"Thank you for your purchase! Your order will be shipped within 3-5 business days");
+
+            }
+
+            
+        }
+    }
+}
